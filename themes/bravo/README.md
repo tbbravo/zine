2c98d130d007b98d97b43dcf46a488ff92da9ad8
yuhuanzhu <yuhuanzhu@baidu.com>
2015-12-10 17:30:01 +0800

    first commit

---
+++ themes/bravo/README.md
@@ -0,0 +1,4 @@
+# weekly
+team weekly of bravo!
+![图片](http://wiki.baidu.com/download/attachments/53026017/tbpc?version=1&modificationDate=1426498968000&api=v2)
+[Bravo周刊](http://fedev.baidu.com/~dengxi/weekly/)
---
+++ themes/bravo/source/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @Author dengxi
+ * @Date 2015-09-10 21:02:46
+ * @Last Modified by:   anchen
+ * @Last Modified time: 2015-09-10 21:02:46
+ */
+
+'use strict';
+$(document).ready(function () {
+    $("[data-toggle='tooltip']").tooltip();
+});
